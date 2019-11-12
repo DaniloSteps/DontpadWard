f314b59bb3c650dc61c65054fb2ec1a7578a5b0c
DaniloSteps <sr.danilop@gmail.com>
2019-11-12 18:10:43 -0300

    new file:   ward.py

---
+++ ward.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+from os import system
+import time
+
+
+def access_dontpad(dontpad):
+    link = requests.get(dontpad)
+    soup = BeautifulSoup(link.text, 'html.parser')
+    text = soup.find('textarea').get_text()
+    buffer = open('buffer.txt', 'w+')
+    buffer.write(text)
+    buffer.close()
+
+
+def log(address, last_time):
+    log_file = open('log.txt', 'a+')
+    log_file.write(
+        '\nhttp://dontpad.com/{} changed at {}.'.format(address, last_time))
+    log_file.close()
+
+
+def main():
+    # print("Enter the dontpad domain you wish to ward:")
+    # warded = input("http://dontpad.com/")
+
+    with open('address.txt') as warded:
+        address = warded.read()
+
+    access_dontpad("http://dontpad.com/{}/".format(address))
+    last_time = time.strftime("%H:%M:%S-%d_%m_%Y", time.localtime())
+    system('mv buffer.txt {}_{}.txt'.format(address, last_time))
+
+    log_file = open('log.txt', 'w+')
+    log_file.write(
+        'Warded \"http://dontpad.com/{}\" at {}'.format(address, last_time))
+    log_file.close()
+
+    while (1):
+        access_dontpad("http://dontpad.com/{}/".format(address))
+
+        with open('{}_{}.txt'.format(address, last_time)) as last_file, open('buffer.txt') as buffer:
+            last_data = last_file.read()
+            buffer_data = buffer.read()
+            if last_data != buffer_data:
+                last_time = time.strftime(
+                    "%H:%M:%S-%d_%m_%Y", time.localtime())
+                system('mv buffer.txt {}_{}.txt'.format(address, last_time))
+                log_file = open('log.txt', 'a+')
+                log_file.write(
+                    '\nhttp://dontpad.com/{} changed at {}.'.format(address, last_time))
+                log_file.close()
+            else:
+                log_file = open('log.txt', 'a+')
+                log_file.write(
+                    '\nNo changes on http://dontpad.com/{} at {}.'.format(address, last_time))
+                log_file.close()                
+
+        time.sleep(5)
+
+
+main()
